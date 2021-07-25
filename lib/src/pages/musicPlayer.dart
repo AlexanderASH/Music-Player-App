e779a26dc19c7e48baaa8b2856ce08049fb235f6
Alexander Sanchez <pcua2014@hotmail.com>
2021-07-25 19:51:27 -0400

    starting with the music player screen

--- lib/src/pages/musicPlayer.dart
+++ lib/src/pages/musicPlayer.dart
@@ -1,4 +1,7 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:music_player_app/main.dart';
 
 
 class MusicPlayerScreen extends StatefulWidget {
@@ -7,13 +10,81 @@ class MusicPlayerScreen extends StatefulWidget {
   _MusicPlayerScreenState createState() => _MusicPlayerScreenState();
 }
 
-class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
+class _MusicPlayerScreenState extends State<MusicPlayerScreen> with WidgetsBindingObserver {
+  Duration duration;
+  Duration position;
+  bool isPlaying = false;
+  IconData btnIcon = Icons.play_arrow;
+
+  BaDumTss instance;
+  AudioPlayer audioPlayer;
+
+  Box box;
+
+  String currentSong = "";
+  String currentCover = "";
+  String currentTitle = "";
+  String currentSinger = "";
+  String url = "";
+
+  @override
+  void initState() { 
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    this.box = Hive.box<String>('myBox');
+    this.instance = getIt<BaDumTss>();
+    this.audioPlayer = this.instance.audio;
+    this.duration = new Duration();
+    this.position = new Duration();
+
+    if (this.box.get('playedOnce') == "false") {
+      setState(() {
+        this.currentCover = "";
+        this.currentTitle = "Choose a song to play";
+      });
+    }
+
+    if (this.box.get('playedOnce') == "true") {
+      this.currentCover = this.box.get('currentCover');
+      this.currentSinger = this.box.get('currentSinger');
+      this.currentTitle = this.box.get('currentTitle');
+      this.url = this.box.get('url');
+    }
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.inactive || state == AppLifecycleState.paused) {
+      this.audioPlayer.pause();
+      setState(() {
+        this.btnIcon = Icons.pause;
+      });
+    }
+
+    if (state == AppLifecycleState.resumed) {
+      if (this.isPlaying) {
+        audioPlayer.resume();
+        setState(() {
+          btnIcon = Icons.play_arrow;
+        });
+      }
+    }
+
+    if (state == AppLifecycleState.detached) {
+      this.audioPlayer.stop();
+      this.audioPlayer.release();
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
-     ),
-   );
+    return Scaffold();
   }
 }
