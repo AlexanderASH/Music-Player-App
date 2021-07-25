76b791156ddcdb884b6ce3934a3d2e53ed3b7757
Alexander Sanchez <pcua2014@hotmail.com>
2021-07-25 09:45:35 -0400

    settings and initial setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,48 @@
+import 'dart:io';
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
+import 'package:music_player_app/src/pages/musicPlayer.dart';
+import 'package:path_provider/path_provider.dart';
  
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setUp();
+  Directory directory = await getApplicationDocumentsDirectory();
+
+  Hive.init(directory.path);
+  await Hive.openBox<String>('myBox');
+  Box box = Hive.box<String>('myBox');
+  if (box.get('playedOnce') == null)  {
+    box.put('playedOnce', "false");
+  }
+  runApp(MyApp());
+}
+
+final getIt = GetIt.instance;
+
+class BaDumTss {
+  AudioPlayer _audio = AudioPlayer();
+
+  AudioPlayer get audio => this._audio;
+}
+
+void setUp() {
+  getIt.registerFactory(() => BaDumTss());
+}
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink
       ),
+      home: MusicPlayerScreen()
     );
   }
 }
---
+++ lib/src/pages/musicPlayer.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class MusicPlayerScreen extends StatefulWidget {
+
+  @override
+  _MusicPlayerScreenState createState() => _MusicPlayerScreenState();
+}
+
+class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hola Mundo'),
+     ),
+   );
+  }
+}
