43183380248723ea0c7b451ac8a3ceedd0e9e32f
Alexander Sanchez <pcua2014@hotmail.com>
2021-07-26 15:51:43 -0400

    model with hive

--- lib/src/models/Music.dart
+++ lib/src/models/Music.dart
@@ -1,21 +1,38 @@
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 import 'dart:typed_data';
 
+@HiveType(typeId: 1)
 class Music {
+  @HiveField(0)
   final String trackName;
+  @HiveField(1)
   final List<dynamic> trackArtistNames;
+  @HiveField(2)
   final String albumName;
+  @HiveField(3)
   final String albumArtistName;
+  @HiveField(4)
   final int trackNumber;
+  @HiveField(5)
   final int albumLength;
+  @HiveField(6)
   final int year;
+  @HiveField(7)
   final String genre;
+  @HiveField(8)
   final String authorName;
+  @HiveField(9)
   final String writerName;
+  @HiveField(10)
   final int discNumber;
+  @HiveField(11)
   final String mimeType;
+  @HiveField(12)
   final int trackDuration;
+  @HiveField(13)
   final int bitrate;
+  @HiveField(14)
   final Uint8List albumArt;
 
   Music({
